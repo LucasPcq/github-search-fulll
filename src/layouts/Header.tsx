0bbc02ffca122b8ddef953be69d14d013e486a76
LucasPcq <picquelucas17@gmail.com>
2023-09-23 14:21:57 +0200

    feat(Header): Create Header Layout & Simple reset CSS

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,5 @@
+import "./styles/header.css";
+
+export const Header = () => {
+  return <header className="header-app">Github Search</header>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./ui/app/App.tsx";
+
+import "./styles/reset.css";
+import "./styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
